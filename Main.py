26b863b1bb1216443bf953190e31a128530601af
Ashutosh Maurya <92643685+ashu1230@users.noreply.github.com>
2023-01-27 18:23:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ from tkinter import Tk, END, Entry, N, E, S, W, Button
 from tkinter import font
 from tkinter import Label
 from functools import partial
-
 def get_input(entry, argu):
     entry.insert(END, argu)
 
