7fd859af4c147fe9d35023b9a2aa7cce6eb66c5e
Ashutosh Maurya <92643685+ashu1230@users.noreply.github.com>
2023-01-27 18:23:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ from tkinter import Label
 from functools import partial
 def get_input(entry, argu):
     entry.insert(END, argu)
-
 def backspace(entry):
     input_len = len(entry.get())
     entry.delete(input_len - 1)
